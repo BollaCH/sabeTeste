573aa3aa9cd23004e7532f8279ec6d0d45a2119a
Diogo Chandoha <50423349+BollaCH@users.noreply.github.com>
2023-10-25 14:09:34 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,25 @@ function copyrightYear() {
 }
 
 copyrightYear();
+
+
+function toggle() {
+    const thanks = document.querySelector(".option-1");
+    const rating = document.querySelector(".option-2");
+    rating.classList.add("hidden");
+    thanks.classList.remove("hidden");
+}
+function toggleD() {
+    const thanks = document.querySelector(".option-2");
+    const rating = document.querySelector(".option-1");
+    rating.classList.add("hidden");
+    thanks.classList.remove("hidden");
+}
+const button = document.querySelector("#btn-1");
+button.addEventListener("click", () => {
+    toggle();
+});
+const button2 = document.querySelector("#btn-2");
+button2.addEventListener("click", () => {
+    toggleD();
+});
